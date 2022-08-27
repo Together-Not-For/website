8e74cd6ffed4e9cbef845bdfe3cd1c841912bdc6
Benji Charles <47843859+TheNextCEO@users.noreply.github.com>
2022-08-27 18:05:18 -0400

    add tailwinds to website (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="px-0 py-8">
       <Head>
         <title>Together, Not For.</title>
         <meta name="description" content="Together, Not For." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1>Together, Not For.</h1>
-        <h2>We do social organizing, data management, and consulting</h2>
-        <h2>Let's chat at info@togethernotfor.com</h2>
+      <main className="min-h-[100vh] px-16 py-0 flex-1 flex flex-col justify-center text-center">
+        <h1 className="text-2xl">Together, Not For.</h1>
+        <h2 className="text-2xl">
+          We do social organizing, data management, and consulting
+        </h2>
+        <h2 className="text-2xl">Let's chat at info@togethernotfor.com</h2>
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 };
